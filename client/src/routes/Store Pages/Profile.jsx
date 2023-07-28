e662ef0b54e0b47329fa6c517bf5f0eef1dc7ec9
Shaked Levy <shakedlv0@gmail.com>
2023-07-28 18:49:04 +0300

    Fixed Password Change

--- client/src/routes/Store Pages/Profile.jsx
+++ client/src/routes/Store Pages/Profile.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom';
 import { Label, TextInput, Avatar, Toast } from 'flowbite-react';
 import api from '../../utils/api';
-import {HiCheck, HiExclamation} from 'react-icons/hi'
+import { HiCheck, HiExclamation } from 'react-icons/hi'
 import { Toaster, toast } from 'react-hot-toast';
 function Profile() {
   const nav = useNavigate();
@@ -41,68 +41,93 @@ function Profile() {
   const [oldPassword, setOldPassword] = useState("")
 
 
- 
-  
+
+
 
   const HandleChangePassword = (e) => {
     e.preventDefault();
 
     var failed = false;
-        
 
-    api.get("Login?password="+oldPassword).then((result) => {
-      if(result.status===200)
-      {
-        if(result.data === user['password'])
-        {
+
+    api.get("Login?password=" + oldPassword).then((result) => {
+      if (result.status === 200) {
+        if (result.data === user['password']) {
           user['password'] = newPassword;
-          api.put('Users',user).then((result)=>{
-            if(result.status!==200)
-            {
-             failed = true;
+          api.put('Users', user).then((result) => {
+            if (result.status !== 200) {
+              failed = true;
             }
 
-          }).catch((ex)=>{
+          }).catch((ex) => {
             failed = true;
           })
         }
-        else
-        {
+        else {
           failed = true;
         }
- 
+
       }
-      else
-      {
+      else {
         failed = true;
       }
-    }).catch((ex)=>{
+    }).catch((ex) => {
       failed = true;
     });
 
     if(failed)
     {
-      notify(<HiExclamation/>,"Failed")
+      notifyFaild();
+
     }
     else
     {
-      notify(<HiCheck/>,"Succsess")
-
+      notifySuccsess();
+      HandleLogout();
     }
+
+
   }
 
 
-  
-const notify = ({ icon, label }) => {
-  toast.custom(
+  const notifySuccsess = () => {
+    toast.custom(
       (t) => (
-          <Toast>
-
-          </Toast>
+        <div id="toast-danger" className="flex items-center w-full max-w-xs p-4 mb-4 text-gray-500 bg-white rounded-lg shadow dark:text-gray-400 dark:bg-gray-800" role="alert">
+          <div className="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 bg-red-100 rounded-lg dark:text-red-200">
+          <HiCheck/>
+          </div>
+          <div className="ml-3 text-sm font-normal"> Password Updated Succsessfuly !</div>
+          <button onClick={() => toast.dismiss(t.id)} type="button" className="ml-auto -mx-1.5 -my-1.5 bg-white text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex items-center justify-center h-8 w-8 dark:text-gray-500 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700" data-dismiss-target="#toast-danger" aria-label="Close">
+            <span className="sr-only">Close</span>
+            <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+              <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
+            </svg>
+          </button>
+        </div>
       ),
       { id: "unique-notification", position: "bottom-right" }
-  );
-}
+    );
+  }
+  const notifyFaild = () => {
+    toast.custom(
+      (t) => (
+        <div id="toast-danger" className="flex items-center w-full max-w-xs p-4 mb-4 text-gray-500 bg-white rounded-lg shadow dark:text-gray-400 dark:bg-gray-800" role="alert">
+          <div className="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 bg-red-100 rounded-lg dark:text-red-200">
+          <HiExclamation/>
+          </div>
+          <div className="ml-3 text-sm font-normal"> Failed to update password !</div>
+          <button onClick={() => toast.dismiss(t.id)} type="button" className="ml-auto -mx-1.5 -my-1.5 bg-white text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex items-center justify-center h-8 w-8 dark:text-gray-500 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700" data-dismiss-target="#toast-danger" aria-label="Close">
+            <span className="sr-only">Close</span>
+            <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+              <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
+            </svg>
+          </button>
+        </div>
+      ),
+      { id: "unique-notification", position: "bottom-right" }
+    );
+  }
 
   return (
     <main className="bg-neutral-50 w-full  pt-14 flex flex-col sm:flex-row items-center justify-around gap-2">
@@ -156,7 +181,7 @@ const notify = ({ icon, label }) => {
             </form>
           </div>
         </>}
-        <Toaster/>
+      <Toaster />
     </main>
 
 
